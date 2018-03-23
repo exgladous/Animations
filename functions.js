f72b250576422cfb6819fa6581e314d92f8c188e
Matthew J Gao <mg00805@srcs.loc>
2018-03-23 12:19:34 -0700

    changed everything

--- functions.js
+++ functions.js
@@ -1,15 +1,11 @@
 var moveButton = document.getElementById("go");
-moveButton.addEventListener("click", moveTemple);
-
-var xpos = 0,
-    ypos = 0;
+moveButton.addEventListener("click", enlargeTemple);
 
 function moveTemple() {
     var direction = document.getElementById("direction").value;
-
-    if (direction == "right") {
-        document.getElementById("temple").className = "moveright"
-
+    if (direction == "up") {
+        var destinationx = xpos + 100;
+        console.log(destinationx);
+        temple.style.left = xpos + "px";
     }
-
 }
