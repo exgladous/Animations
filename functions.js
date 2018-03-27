d745503d0580ff4dace4ef80395a671428f40c9f
Matthew J Gao <mg00805@srcs.loc>
2018-03-27 12:24:17 -0700

    added mouse following

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
-
 var temple = document.getElementById("temple"),
+    mousecoords = document.getElementById("mousecoords"),
     animation = setInterval(spintemple, 5),
     degangle = 0,
     xpos = 0,
@@ -8,4 +8,20 @@ var temple = document.getElementById("temple"),
 function spintemple() {
     degangle++;
     temple.style.transform = "rotatez(" + degangle + "deg) rotatex(" + degangle + "deg) rotatey(" + degangle + "deg)"
+}
+
+document.addEventListener("mousemove", templefollow)
+function templefollow() {
+    var mouseX = event.clientX,
+        mouseY = event.clientY;
+
+    temple.style.left = mouseX + "px";
+    temple.style.top = mouseY + "px";
+}
+
+window.addEventListener("keypress", frequency);
+function frequency() {
+
+    document.getElementById("rainbow").style.animationDuation = "5s"
+
 }
